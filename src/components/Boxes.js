4888554eb2a1dead17e9b3ed2c6779c626b6c954
Sophia S. <sophia0791@icloud.com>
2024-01-08 11:26:20 +0100

    add solution

---
+++ src/components/Boxes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Box from "./Box";
+
+export default function Boxes() {
+  return (
+    <>
+      <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+    </>
+  );
+}
